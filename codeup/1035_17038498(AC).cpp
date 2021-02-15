d21958cf0245e2b581ab3ee218fa195b605aeaa8
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2021-02-15 14:51:18 +0900

    codeup
    출력변환

---
+++ codeup/1035_17038498(AC).cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> hex >> n;
+    cout << oct << n;
+}
---
+++ codeup/1036_17038535(AC).cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char n;
+    cin >> n;
+    
+    cout << static_cast<int>(n);
+}
