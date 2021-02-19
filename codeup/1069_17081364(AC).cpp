7a0b2e106117c0f6b7c96ed20480fcc4a450b993
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2021-02-19 17:27:58 +0900

    codeup

---
+++ codeup/1069_17081364(AC).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    
+    if(a == 'A')
+        cout << "best!!!";
+    else if (a == 'B')
+        cout << "good!!";
+    else if (a == 'C')
+        cout << "run!";
+    else if (a == 'D')
+        cout << "slowly~";
+    else
+        cout << "what?";
+}
---
+++ codeup/1072_17081536(AC).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a,n;
+    cin >> n;
+    vector<int> c;
+    for(int i=0; i< n; i++)
+    {
+        cin >> a;
+        c.push_back(a);
+    }
+    
+    for(int i=0; i< n; i++)
+        cout << c[i] << endl;
+}
